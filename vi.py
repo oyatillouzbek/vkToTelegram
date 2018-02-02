e42c571b22b9fa2f8fc4807d9bfa6bbaadf4e710
Widowan <thesuperartempro@gmail.com>
2018-02-03 00:15:24 +0300

    name conflict fix

--- vi.py
+++ vi.py
@@ -33,10 +33,10 @@ def is_pinned():
 #using wallget api
 def wallget():
     try:
-        wallget = api.wall.get(owner_id=wallid, domain=domain, offset=is_pinned(), count='1', filter='all', extended='0', v='5.69')
+        _wallget = api.wall.get(owner_id=wallid, domain=domain, offset=is_pinned(), count='1', filter='all', extended='0', v='5.69')
     except Exception:
         wallget()
-    return wallget
+    return _wallget
 
 #analyzing attachments
 def attachments():
