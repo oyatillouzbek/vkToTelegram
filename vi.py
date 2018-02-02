d021c0e937d1793adf0805f92e2313d4b5e62db0
Widowan <thesuperartempro@gmail.com>
2018-02-02 21:05:22 +0300

    Try/Except block in vk api calls; updated readme; edited requirements.txt

--- vi.py
+++ vi.py
@@ -1,10 +1,8 @@
 import vk_api
 import datetime
 import cfg
-from eventlet import sleep
+from time import sleep
 import send
-import socket
-import traceback
 
 login = cfg.readcfg('config.yml')['login']
 password = cfg.readcfg('config.yml')['password']
@@ -16,36 +14,50 @@ try:
     vk_session.auth()
     print('[{}] Auth Succesful!'.format(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")))
 except vk_api.AuthError as e:
-    print('[{}] Auth Error!\nTraceback:\n'.format(datetime.strftime("%Y-%m-%d %H:%M:%S", datetime.gmtime())))
+    print('[{}] Auth Error!\nTraceback:\n'.format(datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")))
     print(e)
 api = vk_session.get_api()
 
-def is_pinned(): #searching for pinned post and skipping it via offset parameter if found
-    data = api.wall.get(owner_id=wallid, domain=domain, offset='0', count='1', filter='all', extended='0', v='5.69')
+#searching for pinned post and skipping it via offset parameter if found
+def is_pinned():
+    try:
+        data = api.wall.get(owner_id=wallid, domain=domain, offset='0', count='1', filter='all', extended='0', v='5.69')
+    except Exception:
+        is_pinned()
     try:
         if data['items'][0]['is_pinned']:
             return 1
     except KeyError:
         return 0
 
-def wallget(): #using wallget api
-    wallget = api.wall.get(owner_id=wallid, domain=domain, offset=is_pinned(), count='1', filter='all', extended='0', v='5.69')
+#using wallget api
+def wallget():
+    try:
+        wallget = api.wall.get(owner_id=wallid, domain=domain, offset=is_pinned(), count='1', filter='all', extended='0', v='5.69')
+    except Exception:
+        wallget()
     return wallget
 
-def attachments(): #analyzing attachments
+#analyzing attachments
+def attachments():
     try:
-        if walldata['items'][0]['attachments']: #looking if there any attachment
-            if walldata['items'][0]['attachments'][0]['type'] == 'photo': #if attachment is photo
+        #looking if there any attachment
+        if walldata['items'][0]['attachments']:
+            #if attachment is photo
+            if walldata['items'][0]['attachments'][0]['type'] == 'photo':
                 photosize = ['photo_1280', 'photo_807', 'photo_604', 'photo_130', 'photo_75']
                 for size in photosize:
                     try:
-                        url = walldata['items'][0]['attachments'][0]['photo'][size] #searching for maximum size
+                        #searching for maximum size
+                        url = walldata['items'][0]['attachments'][0]['photo'][size]
                         attachmentType = 'photo'
                         return url, attachmentType
                     except KeyError as e:
                         continue
-            if walldata['items'][0]['attachments'][0]['type'] == 'video': #if attachment is video
-                videoid = walldata['items'][0]['attachments'][0]['video']['id'] #video's id is needed to generate link to the video
+            #if attachment is video
+            if walldata['items'][0]['attachments'][0]['type'] == 'video':
+                #video's id is needed to generate link to the video
+                videoid = walldata['items'][0]['attachments'][0]['video']['id']
                 videoOwner = walldata['items'][0]['attachments'][0]['video']['owner_id']
                 url = 'vk.com/video{}_{}'.format(videoOwner, videoid)
                 attachmentType = 'video'
@@ -63,14 +75,16 @@ def attachments(): #analyzing attachments
                 a = 'a'
                 b = 'b'
                 return a, b
-    except Exception as e: #if no attachments returning useless values
+    #if no attachments returning useless values
+    except Exception as e:
         a = 'a'
         b = 'b'
         return a, b
 
 getcurrid = lambda: walldata['items'][0]['id']
 getwalltext = lambda: walldata['items'][0]['text']
-getpostlink = lambda: 'vk.com/{}?w=wall{}_{}'.format(domain, wallid, getcurrid()) #generating link to the post
+#generating link to the post
+getpostlink = lambda: 'vk.com/{}?w=wall{}_{}'.format(domain, wallid, getcurrid())
 
 lastid = 0
 walldata = wallget()
@@ -89,13 +103,16 @@ while 1==1:
 
     url = attachments()[0]
     lastid = currid
-    message = '{}\n\n{}'.format(getwalltext(), getpostlink()) #final message that will be sent to channel
+    #final message that will be sent to channel
+    message = '{}\n\n{}'.format(getwalltext(), getpostlink())
     attachmentType = attachments()[1]
 
     if attachmentType == 'photo':
         send.sendtextandphoto(url, message)
     elif attachmentType == 'video':
-        hiddenvideo = '<a href="{}"> </a>'.format(url) #hiding link to video into  'alt+255' synmbol. Telegram wont show it in message, but still link will be avaliable on preview
+        #hiding link to video into  'alt+255' synmbol.
+        #Telegram wont show it in message, but still link will be avaliable on preview
+        hiddenvideo = '<a href="{}"> </a>'.format(url)
         tempmsg = '{}{}'.format(hiddenvideo, message)
         send.sendtextandvideo(tempmsg)
         del(hiddenvideo, tempmsg)
